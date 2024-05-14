31e61415e39a594c56361cc1f43e93bfeb3530d1
dhkang501 <kangg5@naver.com>
2024-05-14 15:23:45 +0900

    setting 수정

--- src/App.tsx
+++ src/App.tsx
@@ -56,8 +56,8 @@ table {
 body {
   font-weight: 300;
   font-family: 'Source Sans Pro', sans-serif;
-  background-color:${(props) => props.theme.bgColor};
-  color:${(props) => props.theme.textColor};
+  background-color: white;
+  color: black;
   line-height: 1.2;
 }
 a {
